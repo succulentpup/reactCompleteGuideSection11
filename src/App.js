70f37e37e5795d88bc986aaa46f3c39ac177d6c8
Ganesh Adapa <36997100+succulentpup@users.noreply.github.com>
2021-05-07 16:16:00 +0100

    component CartProvider has been implemented in the store.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import { Fragment, useState } from 'react';
+import { useState } from 'react';
 import Cart from './Components/Cart/Cart';
 import Header from './Components/Layout/Header';
 import Meals from './Components/Meals/Meals';
+import CartProvider from './store/CartProvider';
 
 const App = () => {
   const [cartIsShown, setCartIsShown] = useState(false);
   const showCartHandler = () => setCartIsShown(true);
   const hideCartHandler = () => setCartIsShown(false);
   return (
-    <Fragment>
+    <CartProvider>
       { cartIsShown && <Cart onCloseCart={hideCartHandler}/> }
       <Header onShowCart={showCartHandler}/>
       <main>
         <Meals/>
       </main>
-    </Fragment>
+    </CartProvider>
   );
 };
 
